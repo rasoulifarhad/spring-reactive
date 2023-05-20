24e388e9435a453c31434c2a66f99533859c9de4
Farhad rasouli <rasouli.farhad@gmail.com>
2023-05-20 20:31:59 +0330

    updated

--- stock-app/stock-ui-demo/src/main/java/com/farhad/example/stockui/ChartController.java
+++ stock-app/stock-ui-demo/src/main/java/com/farhad/example/stockui/ChartController.java
@@ -1,5 +1,7 @@
 package com.farhad.example.stockui;
 
+import static javafx.collections.FXCollections.observableArrayList;
+
 import java.util.function.Consumer;
 
 import org.springframework.stereotype.Component;
@@ -8,12 +10,11 @@ import com.farhad.example.stockclient.StockPrice;
 import com.farhad.example.stockclient.WebClientStockClient;
 
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 
 @Component
 public class ChartController implements Consumer<StockPrice> {
@@ -21,7 +22,7 @@ public class ChartController implements Consumer<StockPrice> {
     @FXML
     public LineChart<String,Double> chart;
     private final WebClientStockClient webClientStockClient ;
-    private ObservableList<Data<String,Double>>  seriesData =  FXCollections.observableArrayList();
+    private ObservableList<Data<String,Double>>  seriesData =  observableArrayList();
 
     public ChartController(WebClientStockClient webClientStockClient) {
         this.webClientStockClient = webClientStockClient;
@@ -29,11 +30,13 @@ public class ChartController implements Consumer<StockPrice> {
 
     @FXML
     public void initialize() {
-        ObservableList<XYChart.Series<String,Double>> data = FXCollections.observableArrayList();
-        data.add(new XYChart.Series<>(seriesData));
+        
+        String symbol = "DEMO";
+        ObservableList<Series<String,Double>> data = observableArrayList();
+        data.add(new Series<>(symbol, seriesData));
         chart.setData(data);
 
-        webClientStockClient.pricesFor("DEMO").subscribe(this);
+        webClientStockClient.pricesFor(symbol).subscribe(this);
 
     }
 
@@ -41,7 +44,7 @@ public class ChartController implements Consumer<StockPrice> {
     public void accept(StockPrice stockPrice) {
         Platform.runLater(() -> 
                     seriesData.add(
-                        new XYChart.Data<String,Double>(
+                        new Data<String,Double>(
                                             String.valueOf(stockPrice.getTime().getSecond()), 
                                             stockPrice.getPrice())));
     }
