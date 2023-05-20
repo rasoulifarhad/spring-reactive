cec46b8e720d022443277484eaf99d5a9e8edd26
Farhad rasouli <rasouli.farhad@gmail.com>
2023-05-20 22:49:06 +0330

    updated

--- stock-app/stock-ui-demo/README.md
+++ stock-app/stock-ui-demo/README.md
@@ -1,3 +1,64 @@
 ## Stock UI (javaFX)
 
 ![Stock Chart](images/stock-chart.png)
+
+<br/>
+<br/>
+
+![Stock Chart](images/stock-chart-02.png)
+
+### JavaFX Recap
+
+In general, a ***JavaFX*** application contains one or more `stages` which corresponds ***to windows***. Each stage has a `scene` attached to it. Each `scene` can have an object graph of controls, layouts etc. attached to it, called the `scene graph`. 
+
+![JavaFX Overview](images/javafx-overview-1.png)
+
+
+***Stage***
+
+***Scene***
+
+***Scene Graph***
+
+***Nodes***
+
+All components attached to the scene graph are called nodes. All nodes are subclasses of a JavaFX class called javafx.scene.Node .
+
+There are two types of nodes: Branch nodes and leaf nodes. A branch node is a node that can contain other nodes (child nodes). Branch nodes are also referred to as parent nodes because they can contain child nodes. A leaf node is a node which cannot contain other nodes.
+
+***Controls***
+
+JavaFX controls are JavaFX components which provide some kind of control functionality inside a JavaFX application. For instance, a button, radio button, table, tree etc.
+
+> For a control to be visible it must be attached to the scene graph of some Scene object.  
+
+
+> Example Controls: ***Accordion/Button/CheckBox/ChoiceBox/ColorPicker/ComboBox/DatePicker/Label/ListView/Menu/MenuBar*** ....
+
+***Layouts***
+
+JavaFX layouts are components which contains other components inside them. 
+ 
+> A layout component must be attached to the scene graph of some Scene object to be visible.  
+
+
+>Example Layouts: ***Group/Region/Pane/HBox/VBox/FlowPane***.....
+
+***Nested Layouts***
+
+***Charts***
+
+JavaFX comes with a set of built-in ready-to-use chart components.
+
+> Example Charts: ***AreaChart/BarChart/BubbleChart/LineChart/PieChart***....
+
+***2D Graphics***
+
+***3D Graphics***
+
+***Audio***
+
+***Video***
+
+***WebView***
+
--- stock-app/stock-ui-demo/src/main/java/com/farhad/example/stockui/ChartController.java
+++ stock-app/stock-ui-demo/src/main/java/com/farhad/example/stockui/ChartController.java
@@ -15,39 +15,51 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Component
-public class ChartController implements Consumer<StockPrice> {
+@RequiredArgsConstructor
+public class ChartController  {
     
     @FXML
     public LineChart<String,Double> chart;
     private final WebClientStockClient webClientStockClient ;
-    private ObservableList<Data<String,Double>>  seriesData =  observableArrayList();
-
-    public ChartController(WebClientStockClient webClientStockClient) {
-        this.webClientStockClient = webClientStockClient;
-    }
-
     @FXML
     public void initialize() {
+        String symbol1 = "SYMBOL1";
+        PriceSubscriber priceSubscriber1 = new PriceSubscriber(symbol1);
+        String symbol2 = "SYMBOL2";
+        PriceSubscriber priceSubscriber2 = new PriceSubscriber(symbol2);
         
-        String symbol = "DEMO";
         ObservableList<Series<String,Double>> data = observableArrayList();
-        data.add(new Series<>(symbol, seriesData));
+        data.add(priceSubscriber1.getSeries());
+        data.add(priceSubscriber2.getSeries());
         chart.setData(data);
-
-        webClientStockClient.pricesFor(symbol).subscribe(this);
-
+        
+        webClientStockClient
+                .pricesFor(symbol1)
+                .subscribe(priceSubscriber1);
+        webClientStockClient
+                .pricesFor(symbol2)
+                .subscribe(priceSubscriber2);
     }
 
-    @Override
-    public void accept(StockPrice stockPrice) {
-        Platform.runLater(() -> 
-                    seriesData.add(
-                        new Data<String,Double>(
-                                            String.valueOf(stockPrice.getTime().getSecond()), 
-                                            stockPrice.getPrice())));
-    }
+    private class PriceSubscriber implements Consumer<StockPrice> {
+        @Getter
+        private Series<String,Double> series;
+        private ObservableList<Data<String,Double>>  seriesData =  observableArrayList();
 
-    
+        public PriceSubscriber(String symbol) {
+            series = new Series<>(symbol, seriesData);
+        }
+        @Override
+        public void accept(StockPrice stockPrice) {
+            Platform.runLater(() -> 
+                        seriesData.add(
+                            new Data<String,Double>(
+                                                String.valueOf(stockPrice.getTime().getSecond()), 
+                                                stockPrice.getPrice())));
+        }
+    }
 }
